24d547978d89e6e31139a6f03c7a21f55e6072ac
Fnrzz <faridnurrganteng123@gmail.com>
2026-02-26 09:45:57 +0700

    [PUSH] fixing bug responsif

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,8 +3,6 @@
         <a href="/" class="flex items-center gap-2">
             <span class="text-xl font-bold text-indigo-600 tracking-tight">Kelas Online</span>
         </a>
-
-        {{-- Desktop Navigation --}}
         <div class="hidden md:flex items-center gap-3">
             <a href="{{ route('catalog') }}"
                 class="px-5 py-2 rounded-full text-lg font-semibold bg-indigo-100 text-indigo-700 transition">
@@ -65,7 +63,6 @@
             @endauth
         </div>
 
-        {{-- Mobile Hamburger Button --}}
         <button @click="mobileOpen = !mobileOpen"
             class="md:hidden flex items-center justify-center p-2 rounded-lg hover:bg-gray-100 transition cursor-pointer">
             <i x-show="!mobileOpen" class="bi bi-list text-2xl text-gray-700"></i>
@@ -73,7 +70,6 @@
         </button>
     </div>
 
-    {{-- Mobile Menu --}}
     <div x-show="mobileOpen" x-transition:enter="transition ease-out duration-200"
         x-transition:enter-start="opacity-0 -translate-y-2" x-transition:enter-end="opacity-100 translate-y-0"
         x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 translate-y-0"
